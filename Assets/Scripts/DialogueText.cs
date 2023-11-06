829d0b6a205c76cc026612dec6461fdbdbc76a5a
shabibokiribo <Niahtaylor806@yahoo.com>
2023-11-06 08:45:51 -0500

    commit

---
+++ Assets/Scripts/DialogueText.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Text txtDialogueRose;
+    public TMP_Text txtDialogueCupidTrig;
+    float dialogueType = .08f;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Rose")
+        {
+            StartCoroutine(Dialogue("Collect roses to confess to your crush",txtDialogueRose));
+        }
+
+        if (other.gameObject.tag == "CupidDialogueTrig")
+        {
+            StartCoroutine(Dialogue("Avoid cupid's arrows to stay faithful to your crush", txtDialogueCupidTrig));
+        }
+
+    }
+    public IEnumerator Dialogue(string text, TMP_Text message)
+    {
+        message.text = "";
+        
+
+        foreach (char c in text)
+        {
+            message.text += c;
+            yield return new WaitForSeconds(dialogueType);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

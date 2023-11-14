975cdbece341d26aa3017a0c962e3dd0460419b8
Janokye <145355697+Janokye@users.noreply.github.com>
2023-11-14 01:23:08 -0500

    Update 7- Health Bar Finished!!
    + Health bar (kinda) works!!
    
    + Added a Healthbar script and a TakeDamage script  (basically player another player script for the healthbar but I didn't want to mess up the actual player script}
    
    + Rn the bar doesn't decrease when it gets hit by a heart projectile
    
    + I'm pretty sure the issue is that I have the wrong tag for the OnTriggerEnter2D but idk; I just work here :/

--- Assets/Scripts/Player/TakeDamage.cs
+++ Assets/Scripts/Player/TakeDamage.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class TakeDamage : MonoBehaviour
 {
+
+    public int maxHealth = 5; 
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Heart"))
+            {
+                TakePlayerDamage(1);
+            }
+        }
+   
         
+        /*
+        if(Input.GetKeyDown(KeyCode.Space)) // TESTING when spacebar is pressed take 1 damage
+        {
+            TakePlayerDamage(1);
+        }
+        */
+    }
+
+    void TakePlayerDamage(int damage) // decrease player's health by a number
+    {
+        if (currentHealth <= 5 && currentHealth >= 0) //Player's health must be between 0 and 5 to take damage
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+        }
     }
 }

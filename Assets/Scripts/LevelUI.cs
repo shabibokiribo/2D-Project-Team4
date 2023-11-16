48f600952edc948f69923c9b92813cc89b090e6f
avery <130318502+abailey444@users.noreply.github.com>
2023-11-16 10:07:29 -0500

    added pause button
    also removed useless functions in buttons

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelUI : MonoBehaviour {
+    public GameObject[] pauseItems;
+
+    public void OnClickPause() {
+        Time.timeScale = 0;
+        foreach(Transform child in gameObject.transform) {
+            child.gameObject.SetActive(false);
+        } foreach(GameObject item in pauseItems) {
+            item.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnClickUnpause() {
+        Time.timeScale = 1;
+        foreach(Transform child in gameObject.transform) {
+            child.gameObject.SetActive(true);
+        } foreach(GameObject item in pauseItems) {
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnClickQuit() {
+        SceneManager.LoadScene("MENU");
+    }
+}

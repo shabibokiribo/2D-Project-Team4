e8e725f38b0f2f9b4ac0219b0edd995d9b88407a
avery <130318502+abailey444@users.noreply.github.com>
2023-11-17 10:12:32 -0500

    broken and unfinished for shield

--- Assets/Scripts/Player/Hammer.cs
+++ Assets/Scripts/Player/Hammer.cs
@@ -3,26 +3,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hammer : MonoBehaviour {
     public GameObject hammerAsset;
     public GameObject spawnPointObj;
-    private float spawnShift = 0f;
-    
+    private bool canUse;
+    public Image shieldImage;
+
+    private void Start() {
+        canUse = true;
+    }
+
     private void Update() {
         if(Input.GetKeyDown(KeyCode.E)) {
-            switch(GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX) {
-                case(false):
-                    spawnShift = 1f;
-                    break;
-                case(true):
-                    spawnShift = -1f;
-                    break;
+            if(GameObject.Find("Hammer(Clone)") == null && canUse) {
+                switch(GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX) {
+                    case(false):
+                        canUse = false;
+                        SpawnHammer(1f);
+                        break;
+                    case(true):
+                        canUse = false;
+                        SpawnHammer(-1f);
+                        break;
+                }
             }
-
-            Vector2 _spawnPoint = new Vector2(spawnPointObj.transform.position.x + spawnShift, spawnPointObj.transform.position.y);
-            GameObject hammer = Instantiate(hammerAsset,_spawnPoint,Quaternion.identity) as GameObject;
-            hammer.GetComponent<SpriteRenderer>().sortingOrder = 1;
         }
+    }
+
+    private void SpawnHammer(float spawnShift) {
+        Vector2 _spawnPoint = new Vector2(spawnPointObj.transform.position.x + spawnShift, spawnPointObj.transform.position.y);
+        GameObject hammer = Instantiate(hammerAsset,_spawnPoint,Quaternion.identity) as GameObject;
+        hammer.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        shieldImage.color = new Color32(96,96,96,255);
+        Invoke("ReturnTime",0.75f);
+    }
+
+    private void ReturnTime() {
+        canUse = true;
+        shieldImage.color = new Color32(255,255,255,255);
     }
 }
